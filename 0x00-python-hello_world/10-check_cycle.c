58d4adb5d18dca79832c2ddfb26ef3ac2823dace
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-22 22:01:08 +0200

    Python task 0x00

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if singly linked list has a cycle
+ * @list: List to be provided
+ * Return: 0 if there is no cycle and 1 if cycel is found
+ */
+
+int check_cycle(listint_t *list)
+{
+
+}
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+int check_cycle(listint_t *list);
+/*I would love to do this, Today I am just short on time*/
+#endif
