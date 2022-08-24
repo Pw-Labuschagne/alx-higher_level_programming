e7a188ca99fe04bf1cfc835568870345a04ca574
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-24 12:22:06 +0200

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_cahr_at(str, n):
+    temp = ""
+    c = 0
+    for c in str:
+        if c == n:
+            pass
+        else:
+            temp += c
+        c += 1
+    return temp
