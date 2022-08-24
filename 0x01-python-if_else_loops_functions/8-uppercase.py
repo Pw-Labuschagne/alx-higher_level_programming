5c52213e3d8b57b61eedc689c3cd6cab23d4628a
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-24 11:49:08 +0200

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,5 @@ def uppercase(str):
     for i in range(0, len(str)):
         ascii_val = ord(str[i])
         if ascii_val >= 97 and ascii_val <= 122:
-            print("{}".format(chr(ascii_val - 32)), end=cond)
-        else:
-            if str[i] == '\0':
-                cond = '\n'
-            print("{}".format(str[i]), end=cond)
+            ascii_val -= 32
+        print("{}".format(chr(ascii_val)), end=cond)
