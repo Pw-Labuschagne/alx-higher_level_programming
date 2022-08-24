9529e70b3a08b85b006004291581513f0debbf50
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-24 12:01:23 +0200

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit += (repr(number)[-1])
-    print(last_digit, end="")
+    last_digit = (repr(number)[-1])
+    num += last_digit
+    print(num, end="")
