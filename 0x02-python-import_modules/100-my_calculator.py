4014ca3e9a5b4b47e96fe6610ae89e62f4bf2fe0
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-25 21:52:39 +0200

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
+    from calculator_1 import add, sub, mul, div
 
     num_arg = len(sys.argv) - 1
     operator = sys.argv[2]
