5ac46cba426e4945f6f10efe4f53d5d94d24ddf4
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-25 21:45:19 +0200

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     if num_arg != 3:
         print("Usage: ./100-my_calculator.py <a> operator <b>")
         exit(1)
-    elif operator == +:
+    elif operator == '+':
         print("{:d} {} {:d} = {}".format(a, operator, b, add(a, b)))
-    elif operator == -:
+    elif operator == '-':
         print("{:d} {} {:d} = {}".format(a, operator, b, sub(a, b)))
-    elif operator == *:
+    elif operator == '*':
         print("{:d} {} {:d} = {}".format(a, operator, b, mul(a, b)))
-    elif operator == /:
+    elif operator == '/':
         print("{:d} {} {:d} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
