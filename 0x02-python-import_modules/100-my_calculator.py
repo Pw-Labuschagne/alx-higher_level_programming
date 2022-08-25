542ac17028a185680c630fadef97f1f7a6c13f24
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-25 21:42:47 +0200

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    num_arg = len(sys.argv) - 1
+    operator = sys.argv[3]
+    a = int(sys.argv[1])
+    b = int(sys.argv[4])
+    if num_arg != 3:
+        print("Usage: ./100-my_calculator.py <a> operator <b>")
+        exit(1)
+    elif operator == +:
+        print("{:d} {} {:d} = {}".format(a, operator, b, add(a, b)))
+    elif operator == -:
+        print("{:d} {} {:d} = {}".format(a, operator, b, sub(a, b)))
+    elif operator == *:
+        print("{:d} {} {:d} = {}".format(a, operator, b, mul(a, b)))
+    elif operator == /:
+        print("{:d} {} {:d} = {}".format(a, operator, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
