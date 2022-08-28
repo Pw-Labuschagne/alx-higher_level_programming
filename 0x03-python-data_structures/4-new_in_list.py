e8fe75e7108df12b47cf2093eda6e079908e0de0
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-28 09:23:38 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list_copy = my_lsit[:]
     if (idx < 0 or idx > (len(my_list) - 1)):
-        return (list_copy)
+        return (my_list[:])
     else:
+        list_copy = my_list
         list_copy[idx] = element
         return (list_copy)
