800e49a20426b65e3023d2fb1d770c9b0548a0ab
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-28 09:39:01 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace("cC", "")
-    return (new_string)
+    new_str = my_string.replace("c","")
+    new_str = new_str.replace("C","")
+    return (new_str)
