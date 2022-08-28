f9c7db809c0b467c842e1183d847870831594508
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-28 09:44:08 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = my_string.replace("c", "")
-    new_str = new_str.replace("C", "")
+    new_str = (my_string.translate({ord(i): None for i in 'c'}))
+    new_str = (new_str.translate({ord(i): None for i in 'C'}))
     return (new_str)
