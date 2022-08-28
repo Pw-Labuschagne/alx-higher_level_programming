523167d5136122ba2e8d099e19ec272889158eed
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-28 09:00:59 +0200

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def element_at(my_list, ind):
+    if idx < 0 or idx > len(my_list):
+        return (none)
+    else:
+        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
