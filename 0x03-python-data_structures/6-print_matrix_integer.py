bd0bb67baea1196cbe192ae3bb49ae3456a5231a
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-28 09:56:13 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print(matrix.matrix(A))
+    def print_matrix_integer(matrix=[[]]):
+    for z in range(len(matrix)):
+        for i in range(len(matrix[z])):
+            if i != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[z][i]), end='')
+        print()
