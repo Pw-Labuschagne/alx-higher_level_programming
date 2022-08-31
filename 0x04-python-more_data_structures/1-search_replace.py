7d55e89790bd3e375fd1d0eacdc7c558986c768f
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-31 21:04:14 +0200

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_lsit, search, replace):
+
+    alist = mylist[:]
+
+    for x, y in enumerate(alist):
+        if alist[i] = search:
+            alist[i] = replace
+
+    return alist
