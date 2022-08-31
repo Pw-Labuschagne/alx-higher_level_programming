2bfacde83279af81ebedba7bf6dfc2402a43c24a
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-31 21:27:07 +0200

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    alist = []    
-    for i in matrix:
-        new = list(map(lambda x: x*x, i))
-        alist.append(new)
-return alist
+    return [[y ** 2 for y in x] for x in matrix]
