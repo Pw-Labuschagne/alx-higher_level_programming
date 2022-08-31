baece7da0e1b79c73be46e0359354e85e45fa806
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-31 20:13:08 +0200

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[i ** 2 for i in x] for x in matrix]
+    new = []
+    for i in matrix:
+        new = new.append(sqr(lambda i: i ** 2))
+    return new
