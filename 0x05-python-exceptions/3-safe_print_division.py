91ed40153b92df8710e1bca7f12a0765a20bb28c
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-12 20:31:08 +0200

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,11 +3,11 @@
 
 def safe_print_division(a, b):
     
-    sumAll = 0
+
     try:
         sumAll = a / b
     except ZeroDivisionError:
-       sumAll = (int(None))
+       sumAll = None
     finally:
-        print(f"Inside result: {sumAll}")
+        print("Inside result: {}".format(sumAll))
         return(sumAll)
