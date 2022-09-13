27f100afbd6213206efc27b997347b38be70c98d
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-13 21:54:11 +0200

    0x06-python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -71,8 +71,6 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(self.__size):
-            print("".join(["#" for j in range(self.__size)]))
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
