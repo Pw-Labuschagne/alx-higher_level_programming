d36126c9a1eab609a7e03fda13ffe93dd46b0c96
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-13 20:50:52 +0200

    0x06-python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""No longer an empty class, but now has size"""
+
+
+class Square:
+    """Giving our Square a size attribute"""
+
+    def __init__(self, size):
+        """This is used to initialize the square"""
+        self.__size = size
