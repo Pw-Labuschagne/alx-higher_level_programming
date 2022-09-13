e55d40ec37e9ba7c7cc64d4c17f60e8a923d1ac1
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-13 20:55:48 +0200

    0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Now we take our Square size and give it some value"""
+
+
+class Square:
+    """The Square size will now take int type"""
+
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            return size
+        except ValueError:
+            print("size must be >= 0")
+            return
+        except TypeError:
+            print("size must be an integer")
+            return
