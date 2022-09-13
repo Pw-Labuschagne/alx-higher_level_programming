51a3ac41241c182e87c41b0f61fb140ffda5d983
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-13 21:16:51 +0200

    0x06-python-classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""With this instance we will return the area of the Square"""
+
+
+class Square:
+    """The attributes of the Square"""
+
+    def __intit(self, size=0):
+        """The square as before takes the values
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """Now the magic comes, calculating the area of square
+        Returns:
+            Area of the Square
+        """
+
+        return (self.__size) ** 2
