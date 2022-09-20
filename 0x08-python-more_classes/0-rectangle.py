888085f50fac9be95fbdfd27dc769e167c0ded98
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-20 19:24:16 +0200

    0x08-python-more_classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Creating a rectangle"""
 
 
 class Rectangle:
-    """empty class that defines a rectangle"""
+    """
+    empty class that defines a rectangle
+    """
     def __init__(self):
         pass
