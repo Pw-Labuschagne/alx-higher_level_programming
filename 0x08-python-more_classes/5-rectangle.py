f6ab51a861206ca44ccd11ce694afb4c2b7f879e
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-20 19:26:58 +0200

    0x08-python-more_classes

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Creating a rectangle"""
 
 
 class Rectangle:
-    """empty class that defines a rectangle"""
+    """
+    empty class that defines a rectangle
+    """
+
 
     def __init__(self, width=0, height=0):
         self.width = width
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Creating a rectangle"""
 
 
 class Rectangle:
-    """empty class that defines a rectangle"""
+    """
+    empty class that defines a rectangle
+    """
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
