405b1b4b7ad8b6619ac6b8f4ea805c8dbab18823
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-26 21:43:12 +0200

    0x0A-python-inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
             if value <= 0
                 ValueError: <name> must be greater than 0
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
