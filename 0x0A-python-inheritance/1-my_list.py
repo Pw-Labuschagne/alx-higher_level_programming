948398f81673ccea89544d151dc608624a08cdd0
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-26 20:25:58 +0200

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class which inherits from a list"""
+
+
+class MyList(list):
+
+    def print_sorted(self)
+    """Prints list in ascending order"""
+
+    print(sorted(self))
