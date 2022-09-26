9c7b8174ef2b42d745b808b064cf8163b8c676e8
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-26 20:01:49 +0200

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Used to see the attributes and methos of a class"""
+
+def lookup(obj):
+    """This will display the attributes and methpds of a specified class"""
+    print(dir(obj))
+
