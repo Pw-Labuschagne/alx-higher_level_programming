872b73528304918dc727e16c7280a01601926d29
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-27 20:44:20 +0200

    0x0B-python-input_output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,5 +5,4 @@ import json
 
 def to_json_string(my_obj):
     """Returns JSON rep of obj"""
-    with open(my_obj, mode="w", encoding="utf-8") as j_man:
-        return json.dumps(my_obj)
+    return json.dumps(my_obj)
