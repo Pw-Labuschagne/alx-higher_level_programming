fc1702f9d14ddc893fb92e439833dc7d466d03fe
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-27 20:26:12 +0200

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,10 +5,5 @@
 def write_file(filename="", text=""):
     """Writes to text file(uft-8) and returns characters written"""
     with open(filename, mode="w", encoding="utf-8") as myFile:
-    
-        x = 0
-        for i in text:
-            myFile.write(i)
-            x +=1
-        
-        return x
+
+        return myFile.write(text)
