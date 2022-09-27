ada694a105bd1480ffd1fd80561ed5769c8e16b4
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-27 20:10:16 +0200

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Used to read a text file(UFT-8) and print to STDOUT"""
+
+
+def read_file(filename=""):
+    """Reads a text file UFT-8 and prints to STDOUT"""
+    with open(filename, encoding="utf-8") as myFile:
+
+        print(myFile.read())
