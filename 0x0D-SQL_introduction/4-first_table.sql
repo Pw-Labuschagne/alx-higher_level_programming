5dba15295f7e271be0e1c0e7de59cb996f1d292e
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-01 19:00:42 +0200

    0x0D-SQL_intro

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Used to create a table called first_table in current database
-CREATE IF NOT EXIST `first_table` (`id` INT, `name` VARCHAR(256));
+CREATE IF NOT EXIST first_table (`id` INT, `name` VARCHAR(256));
 
