ed89daaafea6ceaad2780399af9fc8116024b048
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-01 19:01:53 +0200

    0x0D-SQL_intro

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- Used to create a table called first_table in current database
-CREATE IF NOT EXIST first_table (`id` INT, `name` VARCHAR(256));
+-- script that creates a table called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
 
