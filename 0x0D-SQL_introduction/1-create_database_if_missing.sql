25ad029593571ef1441770ea8f48e6d85b6bf3ec
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-01 18:56:47 +0200

    0x0D-SQL_intro

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- Used to create a database named hbtn_0c_0 if it doesn't exist
-CREATE DATABASE IF NOT EXIST `hbtn_0c_0`;
+-- creates the database hbtn_0c_0 in your MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
