bb013327708015f1af86adf3977328f8248d9709
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-02 20:04:52 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates a database named hbtn_0d_2 and user_0d_2
 CREATE IF NOT EXISTS hbtn_0d_2@localhost;
 CREATE IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2pwd';
-GRANT ON hbtn_0d_2.* TO 'user_0d_2';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_pwd';
 
