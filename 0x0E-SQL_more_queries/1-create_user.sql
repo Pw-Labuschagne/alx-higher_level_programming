70773fec33277ddcc4020e784e1816273be03ba2
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-02 19:47:01 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates a user: user_0d_1 with some attributes
-CREATE USER IF NOT EXIST user_0d_01@localhost IDENTIFIED WITH 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_01'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1@localhost';
 
