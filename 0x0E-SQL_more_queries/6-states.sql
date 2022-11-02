b48b87708583d78f47f9794c5cf5f07538822a9d
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-02 20:38:14 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates a database htbn_0d_use and table states in MYSQL server
 CREATE DATABSE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-	id INT SERIAL UNIQUE AUTO NOT NULL PRIMARY KEY,
+	id INT SERIAL UNIQUE NOT NULL PRIMARY KEY,
 	name VARCHAR(256)
 	);
