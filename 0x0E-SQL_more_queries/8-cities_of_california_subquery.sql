ae07022eda8bdc9fec4ecf5c20c8f80367fd473e
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-02 21:06:14 +0200

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Lists all cities of California thah can be found in database hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = "California"
+	);
