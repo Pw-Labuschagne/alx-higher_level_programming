4be5a7e26dfd423c86e35e2115ea3b097179be8c
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-11-02 20:13:02 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- Grant SELECT on user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_pwd@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 
