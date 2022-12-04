3d37bebf37a8ec68a365d08df07e00ca8a806d44
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-12-04 08:42:45 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A script that displays if name matches argument"""
+
+import MySQLdb
+import sys
+
+def Matched():
+    """Prints if it matches"""
+
+    username = sys.arg[1]
+    password = sys.arg[2]
+    D_B = sys.arg[3]
+    searched = sys.argv[4]
+    H_N = "localhost"
+    p_Id = 3306
+
+    db = MySQLdb.connect(host=H_N, user=username, passwd=password, db=D_B, port=p_Id)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = '{:s}'.format(searched)")
+
+    show = cur.fetchall()
+
+    print(show)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    Matched()
