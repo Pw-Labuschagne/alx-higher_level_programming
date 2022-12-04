5ddad8c89db13181a4bb9d3feecaf07a8af56130
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-12-04 08:18:45 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""A script that lists all states in db hbtn_0e_0_usa"""
-
+"""Script"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY ID")
-    Results = cur.fetchall()
+    cur.execute("SELECT DISTINCT * FROM states ORDER BY id")
+    rows = cur.fetchall()
 
-    for row in Results:
+    for row in rows:
         print(row)
 
     cur.close()
