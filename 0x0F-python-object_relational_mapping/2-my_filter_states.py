14f824cd4b15b38938614f1672f0fce3d774fb58
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-12-04 08:47:52 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,8 @@ def Matched():
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE BINARY name = \'{}\'\ ORDER
-                BY id").format(searched)
+    cur.execute(("SELECT * FROM states WHERE BINARY name = \'{}\'\ ORDER
+                BY id").format(searched))
 
     show = cur.fetchall()
 
