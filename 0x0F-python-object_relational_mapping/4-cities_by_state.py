3197e69e6fc703dfec4d529e582f94dcdb24b061
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-12-04 09:35:31 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A scrip to display cities"""
+
+import MySQLdb
+import sys
+
+
+def DisplayCities():
+    """Display cities in db hbtn_0e_4_usa"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    D_B = sys.argv[4]
+    H_N = "localhost"
+    p_Id = 3306
+
+    db = MySQLdb.connect(host=H_N, user=username,
+                         passwd=password, db=D_B, port=p_Id)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM cities ORDER BY id")
+
+    show = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    for i in show:
+        print(i)
+
+
+if __name__ == "__main__":
+    DisplayCities()
