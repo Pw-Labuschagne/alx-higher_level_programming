9609d1096f552fa23e40c5c3b1c04783c077b4c5
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-12-15 08:32:55 -0800

    Python Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL
 
-curl -s -L -I "$1" | grep -i Content-Lenght: | cut -d " " -f2
+curl -s -L -I "$1" | grep -i Content-Lenght | cut -d " " -f2
